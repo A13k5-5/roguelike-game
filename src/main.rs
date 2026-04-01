f30a2e017d988b4f372c489654ed45c433799dd6
alex <pisonalex@gmail.com>
2026-04-01 15:24:02 +0200

    feat: the '@' is moving on the screen
    Toggle fullscreen not working properly

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,32 @@ struct Tcod {
     root: Root,
 }
 
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::*;
+
+    let key = tcod.root.wait_for_keypress(true);
+
+    match key {
+        Key { code: KeyCode::Up, .. } => *player_y -= 1,
+        Key { code: KeyCode::Down, .. } => *player_y += 1,
+        Key { code: KeyCode::Left, .. } => *player_x -= 1,
+        Key { code: KeyCode::Right, .. } => *player_x += 1,
+        Key {
+            code: KeyCode::Enter,
+            alt: true,
+            ..
+        } => {
+            // alt+enter: toggle fullscreen
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(fullscreen);
+        }
+        Key { code: KeyCode::Escape, .. } => return true, // exit the game
+        _ => {}
+    }
+
+    false
+}
+
 fn main() {
     let root = Root::initializer()
         .font("arial10x10.png", FontLayout::Tcod)
@@ -21,11 +47,18 @@ fn main() {
     let mut tcod = Tcod { root };
     tcod::system::set_fps(LIMIT_FPS);
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
     while !tcod.root.window_closed() {
         tcod.root.set_default_foreground(WHITE);
         tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root.put_char(player_x, player_y, '@', BackgroundFlag::None);
         tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
+
+        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
     }
 }
