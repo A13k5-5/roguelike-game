f8dc8c5267bbde5a65da161f2501ab0f2c6fb1ff
alex <pisonalex@gmail.com>
2026-04-02 23:34:28 +0200

    feat: tunnel making to create tunnels btw rooms

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use tcod::colors::*;
 use tcod::console::*;
 
@@ -20,9 +21,10 @@ fn make_map() -> Map {
 
     // create 2 rooms
     let room1 = Rect::new(20, 15, 10, 15);
-    let room2 = Rect::new(60, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
     create_room(room1, &mut map);
     create_room(room2, &mut map);
+    create_h_tunnel(25, 51, 23, &mut map);
 
     map
 }
@@ -82,6 +84,20 @@ fn create_room(room: Rect, map: &mut Map) {
         for y in (room.y1 + 1)..room.y2 {
             map[x as usize][y as usize] = Tile::empty();
         }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    // horizontal tunnel
+    for x in cmp::min(x1, x2)..cmp::max(x1, x2) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    // horizontal tunnel
+    for y in cmp::min(y1, y2)..cmp::max(y1, y2) {
+        map[x as usize][y as usize] = Tile::empty();
     }
 }
 
