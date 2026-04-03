724b8651e9467df2c3f754f825414ae11de31c00
alex <pisonalex@gmail.com>
2026-04-03 23:41:25 +0200

    refactor: map logic refactored to map module

--- src/main.rs
+++ src/main.rs
@@ -6,76 +6,83 @@ use tcod::console::*;
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 
-// size of the map
-const MAP_WIDTH: i32 = 80;
-const MAP_HEIGHT: i32 = 50;
-
 // parameters for dungeon generator
 const ROOM_MAX_SIZE: i32 = 10;
 const ROOM_MIN_SIZE: i32 = 6;
 const MAX_ROOMS: i32 = 30;
 
-type Map = Vec<Vec<Tile>>;
+mod game_map {
+    use rand::RngExt;
+    use crate::{create_h_tunnel, create_room, create_v_tunnel, Object, Rect, Tile, MAX_ROOMS, ROOM_MAX_SIZE, ROOM_MIN_SIZE};
 
-struct Game {
-    map: Map
-}
+    // size of the map
+    pub const MAP_WIDTH: i32 = 80;
+    pub const MAP_HEIGHT: i32 = 50;
 
-fn make_map(player: &mut Object) -> Map {
-    // fill map with wall tiles
-    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    pub type Map = Vec<Vec<Tile>>;
 
-    let mut rooms = vec![];
-
-    for _ in 0..MAX_ROOMS {
-        // random size
-        let w = rand::rng().random_range(ROOM_MIN_SIZE..=ROOM_MAX_SIZE);
-        let h = rand::rng().random_range(ROOM_MIN_SIZE..=ROOM_MAX_SIZE);
-
-        // random position
-        let x = rand::rng().random_range(0..MAP_WIDTH - w);
-        let y = rand::rng().random_range(0..MAP_HEIGHT - h);
-
-        let new_room = Rect::new(x, y, w, h);
-
-        let failed = rooms
-            .iter()
-            .any(|other_room| new_room.intersects_with(other_room));
-
-        if failed {
-            continue
-        }
-
-        let (new_x, new_y) = new_room.centre();
-        create_room(new_room, &mut map);
-
-        match rooms.is_empty() {
-            true => {
-                // this is the first room, where the player is spawned
-                player.x = new_x;
-                player.y = new_y;
-            }
-            false => {
-                // all the other rooms
-                // connect it to the previous room with a tunnel
-
-                let (prev_x, prev_y) = rooms.last().unwrap().centre();
-
-                // horizontal and then vertical or the other way around
-                if rand::random() {
-                    create_h_tunnel(new_x, prev_x, new_y, &mut map);
-                    create_v_tunnel(new_y, prev_y, prev_x, &mut map);
-                } else {
-                    create_v_tunnel(new_y, prev_y, new_x, &mut map);
-                    create_h_tunnel(new_x, prev_x, prev_y, &mut map);
-                }
-            }
-        }
-
-        rooms.push(new_room);
+    pub struct Game {
+        pub map: Map
     }
 
-    map
+    pub fn make_map(player: &mut Object) -> Map {
+        // fill map with wall tiles
+        let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+        let mut rooms = vec![];
+
+        for _ in 0..MAX_ROOMS {
+            // for _ in 0..5 {
+            // random size
+            let w = rand::rng().random_range(ROOM_MIN_SIZE..=ROOM_MAX_SIZE);
+            let h = rand::rng().random_range(ROOM_MIN_SIZE..=ROOM_MAX_SIZE);
+
+            // random position
+            let x = rand::rng().random_range(0..MAP_WIDTH - w);
+            let y = rand::rng().random_range(0..MAP_HEIGHT - h);
+
+            let new_room = Rect::new(x, y, w, h);
+
+            let failed = rooms
+                .iter()
+                .any(|other_room| new_room.intersects_with(other_room));
+
+            if failed {
+                continue
+            }
+
+            let (new_x, new_y) = new_room.centre();
+            create_room(new_room, &mut map);
+
+            match rooms.is_empty() {
+                true => {
+                    // this is the first room, where the player is spawned
+                    player.x = new_x;
+                    player.y = new_y;
+                }
+                false => {
+                    // all the other rooms
+                    // connect it to the previous room with a tunnel
+
+                    let (prev_x, prev_y) = rooms.last().unwrap().centre();
+
+                    // horizontal and then vertical or the other way around
+                    if rand::random() {
+                        create_h_tunnel(new_x, prev_x, new_y, &mut map);
+                        create_v_tunnel(new_y, prev_y, prev_x, &mut map);
+                    } else {
+                        create_v_tunnel(new_y, prev_y, new_x, &mut map);
+                        create_h_tunnel(new_x, prev_x, prev_y, &mut map);
+                    }
+                }
+            }
+
+            rooms.push(new_room);
+        }
+
+        map
+    }
+
 }
 
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
@@ -137,14 +144,10 @@ impl Rect {
             && (self.x2 >= other.x1)
             && (self.y1 <= other.y2)
             && (self.y2 >= other.y1)
-        // (self.x1 <= other.x2)
-        // && (self.x2 >= other.x1)
-        // && (self.y1 <= other.y2)
-        // && (self.y2 >= other.y2)
     }
 }
 
-fn create_room(room: Rect, map: &mut Map) {
+fn create_room(room: Rect, map: &mut game_map::Map) {
     // go through the tiles in the rectangle, and make them passable
     for x in (room.x1 + 1)..room.x2 {
         for y in (room.y1 + 1)..room.y2 {
@@ -153,14 +156,14 @@ fn create_room(room: Rect, map: &mut Map) {
     }
 }
 
-fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut game_map::Map) {
     // horizontal tunnel
     for x in cmp::min(x1, x2)..cmp::max(x1, x2) {
         map[x as usize][y as usize] = Tile::empty();
     }
 }
 
-fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut game_map::Map) {
     // horizontal tunnel
     for y in cmp::min(y1, y2)..cmp::max(y1, y2) {
         map[x as usize][y as usize] = Tile::empty();
@@ -189,7 +192,7 @@ impl Object {
     }
 
     // move by the given amount
-    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+    pub fn move_by(&mut self, dx: i32, dy: i32, game: &game_map::Game) {
 
         // if blocked wall, not possible to get there
         if game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
@@ -207,15 +210,15 @@ impl Object {
     }
 }
 
-fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+fn render_all(tcod: &mut Tcod, game: &game_map::Game, objects: &[Object]) {
     // draw all objects in the list
     for object in objects {
         object.draw(&mut tcod.con);
     }
 
     // go through all the tiles and set their background colour
-    for y in 0..MAP_HEIGHT {
-        for x in 0..MAP_WIDTH {
+    for y in 0..game_map::MAP_HEIGHT {
+        for x in 0..game_map::MAP_WIDTH {
             let wall = game.map[x as usize][y as usize].block_sight;
             if wall {
                 tcod.con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
@@ -228,7 +231,7 @@ fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
     blit(
         &tcod.con,
         (0, 0),
-        (MAP_WIDTH, MAP_HEIGHT),
+        (game_map::MAP_WIDTH, game_map::MAP_HEIGHT),
         &mut tcod.root,
         (0, 0),
         1.0,
@@ -236,7 +239,7 @@ fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
     );
 }
 
-fn handle_keys(tcod: &mut Tcod, player: &mut Object, game: &Game) -> bool {
+fn handle_keys(tcod: &mut Tcod, player: &mut Object, game: &game_map::Game) -> bool {
     use tcod::input::*;
 
     let key = tcod.root.wait_for_keypress(true);
@@ -275,15 +278,15 @@ fn main() {
         .title("Rust/libtcod tutorial")
         .init();
 
-    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
+    let con = Offscreen::new(game_map::MAP_WIDTH, game_map::MAP_HEIGHT);
 
     let mut tcod = Tcod { root, con };
 
     // create object representing the player
     let mut player = Object::new(0, 0, '@', WHITE);
 
-    let game = Game {
-        map: make_map(&mut player)
+    let game = game_map::Game {
+        map: game_map::make_map(&mut player)
     };
 
     // create an NPC
